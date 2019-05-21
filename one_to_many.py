772edc0aa345937b4f332997df2c3c66ccda0d00
matejhumlicek <matmine.omg@gmail.com>
2019-05-21 18:42:36 +0200

    Add files via upload

---
+++ one_to_many.py
@@ -0,0 +1,16 @@
+from flask import flask
+from flask_sqlalchemy import flask_sqlalchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///relationships.db'
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    pets = db.relationship('Pet', backref='owner')
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))   
+    owner_id = db.Column(db.Integer, db.ForeignKey('person.id')) 
